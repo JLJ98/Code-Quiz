0517d979e2a37d69f803976d941c5729cbc3a52f
JLJ98 <jonlukejenkins2@gmail.com>
2023-11-27 17:05:11 -0600

    Added a starter template

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+function submitQuiz() {
+    // Your logic to grade the quiz and display results goes here
+    // You can access selected options using document.querySelector('input[name="q1"]:checked').value
+    // and similar for other questions
+    // Display results to the user or perform any other desired actions
+    alert('Quiz submitted!'); // Placeholder action
+  }
